cbe5ae254ee968c607e15750697a40b834664d60
kimariyb <kimariyb@163.com>
2023-08-03 12:15:14 +0800

    update v1.2.1

--- main.py
+++ main.py
@@ -6,15 +6,21 @@ def welcome():
     """
     主页面信息
     """
-    hello = """
-EasyShermo: A python script to automate the use of Shermo
-Version 1.2.0  Release date: 2023-Jul-28
-Developer: Kimariyb (kimariyb@163.com)
-Address: XiaMen University, School of Electronic Science and Engineering
-Official website: https://github.com/kimariyb/easy-shermo
-    """
-    # 输出主页面信息
-    print(hello)
+    # 定义版本信息
+    version_info = {
+        'version': 'v1.2.1',
+        'release_date': 'Aug-3-2023',
+        'developer': 'Kimariyb, Ryan Hsiun',
+        'address': 'XiaMen University, School of Electronic Science and Engineering',
+        'website': 'https://github.com/kimariyb/kimariPlot',
+    }
+
+    print(f"EasyShermo -- A python script to automate the use of Shermo")
+    print(f"Version: {version_info['version']}, release date: {version_info['release_date']}")
+    print(f"Developer: {version_info['developer']}")
+    print(f"Address: {version_info['address']}")
+    print(f"EasyShermo home website: {version_info['website']}\n")
+
     # 在启动时就读取 settings.ini 并返回对象
     shermo_config = ShermoConfig()
     # 打印配置文件信息
